e60263ed538d3895e1c8cfcd4b808942009000d8
Cezary_Karwoski-Budd <cezary.karwoski-budd@hotmail.com>
2023-06-26 15:49:09 +0100

    testing

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "localhost:3000"
-    origins "thesocialsphere-1915b851fff9.herokuapp.com/"
+    origins "thesocialsphere-1915b851fff9.herokuapp.com"
 
     resource "*",
       headers: :any,
