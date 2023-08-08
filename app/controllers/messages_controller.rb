9a4754281db41891f8c58a1a2e162e7b89d988d2
Cezary_Karwoski-Budd <cezary.karwoski-budd@hotmail.com>
2023-08-08 16:59:07 +0100

    messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,9 +5,9 @@ class MessagesController < ApplicationController
   def index
     # User is found and stored in the id @user before this actions starts
     # Then the other user is found so the messages between the two can be found.
-    got_messages = @user.received_messages.find_by(user_id: friend_user.id)
-    sent_messages = @user.sent_messages
-    render json: {messagesSent: sent_messages , messagesReceived: got_messages}
+    # got_messages = @user.received_messages.find_by(user_id: friend_user.id)
+    # sent_messages = @user.sent_messages
+    # render json: {messagesSent: sent_messages , messagesReceived: got_messages}
   end
 
   def show
@@ -15,7 +15,12 @@ class MessagesController < ApplicationController
   end
 
   def create
+    # Create the message and save it to the database
+    message = Message.create(message_params)
 
+    # Broadcast the message to both users' ConversationChannel
+    ConversationChannel.broadcast_to(message.user, message: message.content, user: message.user.username)
+    ConversationChannel.broadcast_to(message.recipient, message: message.content, user: message.user.username)
   end
 
   def update
@@ -32,4 +37,7 @@ class MessagesController < ApplicationController
     @user = User.find(params[:user_id])
   end
 
+  def message_params
+    params.require(:message).permit(:content, :user_id, :recipient)
+  end
 end
